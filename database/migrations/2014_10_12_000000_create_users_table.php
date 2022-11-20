30719d70f6217e84ba71f32bb62b1fc817ca3312
Geekers-Joel237 <79468070+Geekers-Joel237@users.noreply.github.com>
2022-11-20 05:56:23 +0100

    project structure setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('phone')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->text('address')->nullable();
+            $table->enum('role',['super','admin','user'])->default('user');
+            $table->integer('licence')->nullable();
+            $table->integer('actual_licence_number')->nullable();
+            $table->enum('status',['active','inactive'])->default('active');
             $table->timestamps();
+            $table->rememberToken();
         });
     }
 
