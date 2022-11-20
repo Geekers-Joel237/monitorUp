e3df860338b04a9767d3304531049d379ab64675
Geekers-Joel237 <79468070+Geekers-Joel237@users.noreply.github.com>
2022-11-20 07:31:33 +0100

    setup migrations moddels and controllers basics

---
+++ app/Http/Controllers/ReserverController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Reserver;
+use Illuminate\Http\Request;
+
+class ReserverController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Reserver  $reserver
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Reserver $reserver)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Reserver  $reserver
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Reserver $reserver)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Reserver  $reserver
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Reserver $reserver)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Reserver  $reserver
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Reserver $reserver)
+    {
+        //
+    }
+}
